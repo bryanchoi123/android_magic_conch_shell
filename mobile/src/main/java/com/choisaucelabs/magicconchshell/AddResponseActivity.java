9045eaf787e2a872c8dc8d075fd3ab09433f1763
Bryan Choi <bryanchoi123@gmail.com>
2015-10-21 00:25:21 -0400

    Finished UI portion of adding a new response and added quick functionality for "clear" button in the activity

--- mobile/src/main/java/com/choisaucelabs/magicconchshell/AddResponseActivity.java
+++ mobile/src/main/java/com/choisaucelabs/magicconchshell/AddResponseActivity.java
@@ -4,14 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddResponseActivity extends AppCompatActivity
 {
+    private EditText responseText, responseFile;
+    private Button browse, submit, clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_response);
+
+        responseText = (EditText)findViewById(R.id.response_text);
+        responseFile = (EditText)findViewById(R.id.response_file_name);
     }
+
+    public void clearFields(View v)
+    {
+        responseText.setText("");
+        responseFile.setText("");
+    }
+
 }
